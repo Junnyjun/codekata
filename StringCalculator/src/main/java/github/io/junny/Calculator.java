1908855c79498bb2d0a43ac97060f146c065f317
Junnyjun <chbe5082@naver.com>
2022-07-19 23:53:05 +0900

    feat(문자열 계산김): 계산김

--- StringCalculator/src/main/java/github/io/junny/Calculator.java
+++ StringCalculator/src/main/java/github/io/junny/Calculator.java
@@ -3,17 +3,12 @@ package github.io.junny;
 import java.util.List;
 
 public class Calculator {
-    private final List<Integer> operandList;
-    private final List<OperandEnum> operatorList;
+    private Long result;
 
 
-    public Calculator(List<Integer> operandList, List<OperandEnum> operatorList) {
-        this.operandList = operandList;
-        this.operatorList = operatorList;
-    }
-
-    public void addOperand(Integer operand){
-        operandList.add(operand);
+    public void addOperand(String expression){
+        expression.
+        this.result = calculating(expression);
     }
 
 }
