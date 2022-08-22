9115c8d64451bce25a2ab8c9d692f2e8e931370a
Junnyjun <chbe5082@naver.com>
2022-08-22 23:20:39 +0900

    feat()

--- StringCalculator/src/main/java/github/io/junny/Main.java
+++ StringCalculator/src/main/java/github/io/junny/Main.java
@@ -1,44 +1,15 @@
 package github.io.junny;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        OutOfMemoryList list = new OutOfMemoryList();
-
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-            list.remove(i);
-        }
-//        IntStream.range(0, 10)
-//                .forEach(element -> {
-//                    list.add(element);
-//                    list.remove(element);
-//                });
-
-        list.get(9);
     }
+
 
 }
 
-class OutOfMemoryList {
-    private ArrayList<String> list = new ArrayList<>();
-
-    public void add(int value) {
-        list.add(new String("ABCDEFGHQWERTZXCV" + value));
-    }
-
-    public void remove(int value) {
-        String s = list.get(value);
-        s = null;
-    }
-
-    public void get(int value) {
-        Object data = list.get(value);
-
-        System.out.println("data = " + data);
-    }
-}
 
